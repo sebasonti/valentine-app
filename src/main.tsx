30ab5093e097a107e266d5946cfc79d9c22a1a60
Juan Sebastián Ontiveros Gonzales <sebas.onti94@gmail.com>
2026-02-14 19:41:06 -0600

    Fix React Router basename for github pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/valentine-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
